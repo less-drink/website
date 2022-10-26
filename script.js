ce1db29d3a1082e8b994fc2b291f0adc30a6dff1
Fabian Bandini <91336333+fabianbandini@users.noreply.github.com>
2022-10-26 21:48:00 +0200

    disable contact form

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const notifyContainer = document.getElementById('notifyContainer');
 const notifyMeButton = document.getElementById('notifyMeButton');
 const closeButton = document.getElementById('close');
 
+/*
 notifyMeButton.onclick = () => {
     notifyContainer.classList.remove('inactive');
     notifyContainer.classList.add('active');
@@ -16,4 +17,4 @@ closeButton.onclick = () => {
 notifyUpper.onclick = () => {
     notifyContainer.classList.add('inactive');
     notifyContainer.classList.remove('active');
-}
+}*/
