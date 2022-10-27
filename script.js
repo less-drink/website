f3caff8abc4d73afc5c27c1fcb20f9835a347be4
Fabian Bandini <fabian.bandini.bd@gmail.com>
2022-10-27 16:49:54 +0200

    start implementig logic

--- script.js
+++ script.js
@@ -2,6 +2,27 @@ const notifyUpper = document.getElementById('notifyUpper');
 const notifyContainer = document.getElementById('notifyContainer');
 const notifyMeButton = document.getElementById('notifyMeButton');
 const closeButton = document.getElementById('close');
+const notfiyMain = document.getElementById('notifyMain');
+
+const inputName = document.getElementById('notify-input-name');
+const inputEmail = document.getElementById('notify-input-email');
+
+function validateEmail(){
+    let email = inputEmail.value;
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if(email == ""){
+        //email is empty
+        return;
+    }
+
+    if (email.match(validRegex)) {
+        //email is valid
+    }else {
+        //email is invalid
+    }
+}
+
 
 notifyMeButton.onclick = () => {
     notifyContainer.classList.remove('inactive');
@@ -17,3 +38,5 @@ notifyUpper.onclick = () => {
     notifyContainer.classList.add('inactive');
     notifyContainer.classList.remove('active');
 }
+
+notfiyMain.onclick = validateEmail;
