0822cff35721180198281b9275328be509b66a6b
Fabian Bandini <dev@less-drink.com>
2022-10-27 18:33:25 +0200

    implement validation for email and name

--- script.js
+++ script.js
@@ -7,22 +7,46 @@ const notfiyMain = document.getElementById('notifyMain');
 const inputName = document.getElementById('notify-input-name');
 const inputEmail = document.getElementById('notify-input-email');
 
+const errorName = document.getElementById('error-name');
+const errorEmail = document.getElementById('error-email');
+
 function validateEmail(){
     let email = inputEmail.value;
     var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
     if(email == ""){
-        //email is empty
+        errorEmail.innerText = "Email darf nicht leer sein!"
+        errorEmail.classList.add("active");
+        errorEmail.classList.remove("inactive");
         return;
     }
 
     if (email.match(validRegex)) {
-        //email is valid
+        errorEmail.classList.remove("active");
+        errorEmail.classList.add("inactive");
     }else {
-        //email is invalid
+        errorEmail.innerText = "Ist keine Email!";
+        errorEmail.classList.add("active");
+        errorEmail.classList.remove("inactive");
     }
 }
 
+function validateName(){
+    let name = inputName.value;
+
+    if(name == ""){
+        errorName.classList.add("active");
+        errorName.classList.remove("inactive");
+    }else {
+        errorName.classList.remove("active");
+        errorName.classList.add("inactive");
+    }
+}
+
+function validate(){
+    validateName();
+    validateEmail();
+}
 
 notifyMeButton.onclick = () => {
     notifyContainer.classList.remove('inactive');
@@ -39,4 +63,4 @@ notifyUpper.onclick = () => {
     notifyContainer.classList.remove('active');
 }
 
-notfiyMain.onclick = validateEmail;
+notfiyMain.onclick = validate;
